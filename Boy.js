785f9175a83847a4d754bae2e37d7aa5001eb31b
Xiaoshan <xiaoshan1215@gmail.com>
2017-12-26 18:03:43 +0800

    自定义 NavigationBar

---
+++ Boy.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import {
+    Text,
+    StyleSheet,
+    View
+} from 'react-native';
+import Girl from './Girl';
+import NavigationBar from './NavigationBar';
+
+export default class Boy extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            word: ''
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <NavigationBar
+                title={'boy'}/>
+                <Text style={styles.text}>I am boy.</Text>
+                <Text style={styles.text}
+                      onPress={() => {
+                          this.props.navigator.push({
+                              component: Girl,
+                              params: {
+                                  word: '一枝玫瑰',
+                                  onCallBack: (word) => {
+                                      this.setState({word: word})
+                                  }
+                              }
+                          })
+                      }}>送给女孩一支玫瑰</Text>
+                <Text style={styles.text}>{this.state.word}</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'gray',
+    },
+    text: {
+        fontSize: 20,
+    }
+});
